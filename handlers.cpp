34f4f892087583d8e3e14f9f93187cbbe2e23c6f
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2017-01-18 22:59:39 -0800

    Make file serving handler canonicalize the path before passing it to the file repository

--- handlers.cpp
+++ handlers.cpp
@@ -1,11 +1,19 @@
 #include "handlers.h"
+#include "util.h"
 
 using std::shared_ptr;
+using std::string;
 
 FileServingHttpHandler::FileServingHttpHandler(shared_ptr<FileRepository> repository) : repository(repository) {}
 
 HttpResponse FileServingHttpHandler::handle_request(const HttpRequest& request) {
-    shared_ptr<File> file = repository->get_file(request.uri);
+    // TODO: test this or move it into repository code
+    string path = canonicalize_path(request.uri);
+    if (path == "") {
+        return not_found_response();
+    }
+
+    shared_ptr<File> file = repository->get_file(path);
 
     if (file == NULL) {
         return not_found_response();
